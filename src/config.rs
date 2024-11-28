2ac4b23747a1c8f8b28543f66220039bc0a542bd
nalabelle <git@nalabelle.com>
2024-11-28 23:08:05 +0000

    feat: add prefix to env variables and add template

--- src/config.rs
+++ src/config.rs
@@ -16,23 +16,23 @@ pub struct Config {
 impl Config {
     pub fn from_env() -> Result<Self, env::VarError> {
         Ok(Config {
-            dns_server: env::var("DNS_SERVER")?,
-            dns_zone: env::var("DNS_ZONE")?,
-            key_name: env::var("KEY_NAME")?,
-            key_alg: env::var("KEY_ALG").unwrap_or_else(|_| "hmac-sha256".to_string()),
-            key_file: env::var("KEY_FILE")
+            dns_server: env::var("DNS_UPDATE_DNS_SERVER")?,
+            dns_zone: env::var("DNS_UPDATE_DNS_ZONE")?,
+            key_name: env::var("DNS_UPDATE_KEY_NAME")?,
+            key_alg: env::var("DNS_UPDATE_KEY_ALG").unwrap_or_else(|_| "hmac-sha256".to_string()),
+            key_file: env::var("DNS_UPDATE_KEY_FILE")
                 .unwrap_or_else(|_| "/run/secrets/rfc2136-secret".to_string()),
-            ttl: env::var("TTL")
+            ttl: env::var("DNS_UPDATE_TTL")
                 .unwrap_or_else(|_| "300".to_string())
                 .parse()
                 .unwrap_or(300),
             check_interval: Duration::from_secs(
-                env::var("CHECK_INTERVAL")
+                env::var("DNS_UPDATE_CHECK_INTERVAL")
                     .unwrap_or_else(|_| "300".to_string())
                     .parse()
                     .unwrap_or(300),
             ),
-            lookup_hostname: env::var("LOOKUP_HOSTNAME")?,
+            lookup_hostname: env::var("DNS_UPDATE_LOOKUP_HOSTNAME")?,
         })
     }
 }
